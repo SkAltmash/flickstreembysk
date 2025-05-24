585d0eb9a98ca92c652279ae3e69ffb080f8690a
Altamash sheikh <99234921+SkAltmash@users.noreply.github.com>
2025-05-24 22:05:15 +0530

    Update tmdb.js

--- netlify/functions/tmdb.js
+++ netlify/functions/tmdb.js
@@ -1,25 +1,6 @@
-// netlify/functions/tmdb.js
-const fetch = require('node-fetch');
-
-exports.handler = async function(event) {
-  const API_KEY = process.env.d1becbefc947f6d6af137051548adf7f;
-  const path = event.queryStringParameters.path || '';
-  const query = event.queryStringParameters.query || '';
-
-  const url = `https://api.themoviedb.org/3/${path}?api_key=${API_KEY}&${query}`;
-
-  try {
-    const res = await fetch(url);
-    const data = await res.json();
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-      headers: { 'Content-Type': 'application/json' }
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message })
-    };
-  }
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello from TMDB proxy!" }),
+  };
 };
